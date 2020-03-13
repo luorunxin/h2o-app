e33851ac27f7b628718a2fb4521b0ec937367548
Luorunxin <annlocn@yeah.net>
2020-03-13 14:44:04 +0800

    init fastclick

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,20 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
         h2o
       </h1>
-      <h2 class="subtitle">
-        h2o app
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+
+    }
   }
 }
 </script>
@@ -51,19 +32,9 @@ export default {
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
     display: block;
     font-weight: 400;
-    font-size: 42px;
+    font-size: 1rem;
     color: #FF8000;
     letter-spacing: 1px;
-  }
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-  .links {
-    padding-top: 15px;
   }
 }
 </style>
